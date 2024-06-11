0dc52b0587d51a214edfdebc4a3810317056e156
Gytis Norutis <gytis.norutis@outlook.com>
2024-06-11 19:45:30 +0100

    start of the project

---
+++ AngularProject/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home'
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
+        title: 'About'
+    },
+];
+
+
